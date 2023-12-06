1b6be5e53f398ac79c1ce0a7d9c88a89887ea746
Wesley <wesley.santos140300@gmail.com>
2023-12-05 22:42:19 -0300

    feat: mudança API

--- main.js
+++ main.js
@@ -1,30 +1,70 @@
-const url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9ef03c45cc674844b98e279d1213121e';
+const getApi = (url, callback) => {
+    fetch(url).then(
+        (response) => response.json(),
+        (error) => console.error(error)
+    ).then(
+        data => callback(data),
+        erro => console.error(erro)
+    )
+}
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        // Manipule os dados conforme necessário
-        console.log(data)
-        news(data);
-    })
-    .catch(error => {
-        console.error('Erro ao buscar os dados:', error);
-    });
+function convertDate(datetime) {
+    const data = new Date(datetime);
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        timeZone: 'America/Sao_Paulo'
+    };
 
+    const formatoBR = new Intl.DateTimeFormat('pt-BR', options).format(data);
+    return formatoBR;
+}
 
-function news(data) {
-    const news = document.getElementById('news')
+const newsHeader = (data) => {
+    const news = document.getElementById('box-news-header')
+    const boxNewsHeader = document.getElementsByClassName('box-news-header')
 
-    data.articles.map((item) => {
+    data.results.slice(0, 3).map((item, i) => {
         const div = document.createElement('div')
-        div.classList.add('box-news')
+        div.classList.add('col-md-4')
         div.innerHTML = `
-            <h3>${item.title}</h3>
-            <i><b>Author:</b> ${item.author} - <b>Fonte:</b> ${item.source.name}</i>
-            <p>${item.content}</p>
-            <img src="${item.urlToImage}" class="w-100"/>
+            <div class="box-news-header m-2">
+                <div class="info-news-header">
+                    <h2>${item.name}</h2>
+                </div>
+            </div>
         `;
-
         news.append(div)
+        boxNewsHeader[i].style.backgroundImage = `url("${item.background_image}")`
     })
 }
+
+const newsBody = (data) => {
+    const news = document.getElementById('box-news-body')
+    const boxNewsBody = document.getElementsByClassName('box-news-body')
+
+    data.results.slice(0, 2).map((item, i) => {
+        const div = document.createElement('div')
+        div.classList.add('col-md-6')
+        div.innerHTML = `
+            <div class="box-news-body m-2">
+                <div class="info-news-body">
+                    <h2>${item.name}</h2>
+                </div>
+            </div>
+        `;
+        news.append(div)
+        boxNewsBody[i].style.backgroundImage = `url("${item.background_image}")`
+    })
+}
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    const keyTest = '975632c0c3dc4a3ba4ed3aab3a1d9b24';
+    const urlApiHeader = 'https://api.rawg.io/api/games?key=' + keyTest;
+    const urlApiBody = 'https://api.rawg.io/api/games?key=' + keyTest;
+
+
+    getApi(urlApiHeader, newsHeader)
+    getApi(urlApiBody, newsBody)
+})
